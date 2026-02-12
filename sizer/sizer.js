63d7599308df4819e3aa4efc580497c1378fe93c
Neil Bird <nebird@microsoft.com>
2026-02-12 17:53:42 +0000

    Sizer: update 2-way mirror note with fault domain detail

--- sizer/sizer.js
+++ sizer/sizer.js
@@ -1942,7 +1942,7 @@ function updateSizingNotes(nodeCount, totalVcpus, totalMemory, totalStorage, res
         // Resiliency note
         const resiliencyNames = {
             'simple': 'Simple (no redundancy, 1x raw storage)',
-            '2way': 'Two-way mirror (50% efficiency), performant and resilient to one node failure',
+            '2way': 'Two-way mirror (50% efficiency for two copies of data), performant and resilient to one fault domain (node) failure',
             '3way': 'Three-way mirror (33% efficiency for three copies of data), most performant and resilient to two fault domain (nodes) failures',
             '4way': 'Four-way mirror (4x raw storage, 25% efficiency)'
         };
